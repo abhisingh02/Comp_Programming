710dc05208b8b3617baa8c8595209158329bb126
Abhishek Singh <singh.abhishek.2017447@gmail.com>
2023-05-26 00:06:11 +0530

    Add files via upload

---
+++ Basic Math/all_divisor.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void divisor(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<i<<" ";
+        }
+    }
+    cout<<"\n";
+}
+
+int main()
+{
+    divisor(36);
+}
